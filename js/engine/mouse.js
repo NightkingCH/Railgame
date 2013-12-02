d481f8ee6e6f7291b71605b93e09259e532fdb52
NightkingCH <thomas.erni@nk-s.ch>
2013-12-02 15:26:17 +0100

    mouse state added

--- js/engine/mouse.js
+++ js/engine/mouse.js
@@ -14,7 +14,17 @@
 			
 			currentLastMouseClickPositon = { x: event.clientX - rect.left, y: event.clientY - rect.top };
 		},
+		isMouseDown: false,
+		mouseDown: function(event){
+			mouse.isMouseDown = true;
+		},
+		mouseUp: function(event){
+			mouse.isMouseDown = false;
+		},
 		start: function(){
+			$(mouse.graphics.canvas()).on('mousedown', mouse.mouseDown);
+			$(mouse.graphics.canvas()).on('mouseup', mouse.mouseUp);
+			
 			$(mouse.graphics.canvas()).on('mousemove', mouse.calcCurrentPosition);
 			$(mouse.graphics.canvas()).on('mouseup', mouse.calcLastClickPosition);
 		},
